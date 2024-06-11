47d797f3997f75c694131de0a8ecc6788470ea48
Ikonicity Airban <ikonicityairban@gmail.com>
2024-06-11 19:30:20 +0100

    first commit

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MovieCard({ title, desc, img, no_likes, id }) {
+  return (
+    <div>
+      <div className="top">
+        <div className="img">
+          <img src="vite.svg" alt="img" />
+        </div>
+      </div>
+      <div className="bottom">
+        
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
